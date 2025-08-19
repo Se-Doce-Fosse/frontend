924942526d3288aa976d2a26e806048f70732fd6
jpsalles21 <joaopedrosalles.dev@gmail.com>
2025-08-19 17:37:47 -0300

    chore: exemplo de uso da lib de graficos

--- src/components/HelloWorld/HelloWorld.tsx
+++ src/components/HelloWorld/HelloWorld.tsx
@@ -1,3 +1,4 @@
+import { Chart } from 'react-google-charts';
 import styles from './HelloWorld.module.scss';
 
 export interface HelloWorldProps {
@@ -13,6 +14,19 @@ export const HelloWorld = ({
 }: HelloWorldProps) => {
   const message = showGreeting ? `Hello, ${name}!` : `Goodbye, ${name}!`;
 
+  const data = [
+    ['Task', 'Hours per Day'],
+    ['Work', 9],
+    ['Eat', 2],
+    ['Commute', 2],
+    ['Watch TV', 2],
+    ['Sleep', 7],
+  ];
+
+  const options = {
+    title: 'My Daily Activities',
+  };
+
   return (
     <div
       className={`${styles.helloWorld} ${className}`}
@@ -22,6 +36,14 @@ export const HelloWorld = ({
       <p className={styles.helloWorldSubtitle}>
         Welcome to our React + TypeScript + Vite project!
       </p>
+
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={'100%'}
+        height={'400px'}
+      />
     </div>
   );
 };
