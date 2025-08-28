b30f6024848872d8a4e208c20d6fd5bc2f1445eb
Brenda Pereira <brendacamara@outlook.com>
2025-08-28 10:17:23 -0300

    feat: criando compoenente botao

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { HelloWorld } from '@components';
+import AddToCartButton from './components/AddToCartButton/AddToCartButton';
 
 function App() {
   return (
     <div>
       <HelloWorld name="Se Doce Fosse" />
+      <AddToCartButton />
     </div>
   );
 }
