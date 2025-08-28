b1c406de3c465876c3b34974ab9a05eceea75abc
jpsalles21 <joaopedrosalles.dev@gmail.com>
2025-08-28 12:02:56 -0300

    feat: adiciona suporte a id com fallback para useId

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,3 +1,4 @@
+import { useId } from 'react';
 import styles from './Input.module.scss';
 
 export interface InputProps
@@ -6,12 +7,25 @@ export interface InputProps
   hasBorder?: boolean;
 }
 
-export const Input = ({ label, hasBorder = false, ...props }: InputProps) => {
+export const Input = ({
+  label,
+  hasBorder = false,
+  id,
+  ...props
+}: InputProps) => {
+  const generatedId = useId();
+  const inputId = id ?? generatedId;
+
   return (
     <div className={styles.input}>
-      <label className={styles.inputLabel}>{label}</label>
+      {label && (
+        <label className={styles.inputLabel} htmlFor={inputId}>
+          {label}
+        </label>
+      )}
       <input
-        className={hasBorder ? styles.inputBorder : styles.inputField}
+        id={inputId}
+        className={`${styles.inputField} ${hasBorder ? styles.inputBorder : ''}`}
         {...props}
       />
     </div>
