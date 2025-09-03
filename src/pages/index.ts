03b13a57bed6f6cdef51bfe92d5cf56a9e8a5bf2
regisamxjr <regisamxjr@gmail.com>
2025-09-03 10:39:50 -0300

    feat/107: PR ajustado

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,6 @@
-export { default as Home } from './Home/Home';
-export { default as Dashboard } from './Dashboard';
-export { default as Estoque } from './Estoque';
-export { default as Produtos } from './Produtos';
-export { default as Pedidos } from './Pedidos';
-export { default as Comentarios } from './Comentarios';
-export { default as Configuracoes } from './Configuracoes';
+export { Dashboard } from './Dashboard';
+export { Estoque } from './Estoque';
+export { Produtos } from './Produtos';
+export { Pedidos } from './Pedidos';
+export { Comentarios } from './Comentarios';
+export { Configuracoes } from './Configuracoes';
