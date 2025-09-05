6db211d090396afd518e8d52578d6edc1f119725
Dudu-Eichner <eduardofelbereichner@gmail.com>
2025-09-05 18:46:36 -0300

    fix: altera import do componente Input

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Input } from '@components';
+import { Input } from '../../components/Input';
 import styles from './Login.module.scss';
 
 const Login = () => {
