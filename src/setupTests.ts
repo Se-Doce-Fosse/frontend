6531c6c341ed2bd5184bf9240868c97f569b2a2b
Brenda Pereira <brendacamara@outlook.com>
2025-09-11 00:02:20 -0300

    chore: arrumando configurações para os testes

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,16 @@
+// Polyfill para TextEncoder/TextDecoder no ambiente de testes do Jest
+import { TextEncoder, TextDecoder } from 'util';
+if (typeof global.TextEncoder === 'undefined') {
+  // @ts-expect-error: Jest pode não ter TextDecoder, então fazemos o polyfill
+  global.TextEncoder = TextEncoder;
+}
+if (typeof global.TextDecoder === 'undefined') {
+  // @ts-expect-error: Jest pode não ter TextDecoder, então fazemos o polyfill
+  global.TextDecoder = TextDecoder;
+}
 import '@testing-library/jest-dom';
 
-// Adicionar configurações globais para Jest se necessário
+// Configurações globais para Jest
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
   value: jest.fn().mockImplementation((query) => ({
