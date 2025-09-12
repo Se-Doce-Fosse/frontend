ad207ff35aab474de4a5c02b6a459c52abfa11b6
Dudu-Eichner <eduardofelbereichner@gmail.com>
2025-09-11 22:18:50 -0300

    feat: adiciona componentes na página de produtos

--- src/pages/Produtos/Produtos.tsx
+++ src/pages/Produtos/Produtos.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import style from './Produtos.module.scss';
 import AdminLayout from '../../layouts/AdminLayout/AdminLayout';
+import { Filter } from '../../components/Filter';
+import TableAdminComponent from '../../components/TableAdminComponent/TableAdminComponent';
+
+const status = [
+  { label: 'Todos os status', value: 'todos' },
+  { label: 'Ativos', value: 'ativos' },
+  { label: 'Inativos', value: 'inativos' },
+];
 
 const Produtos: React.FC = () => {
   return (
     <AdminLayout>
-      <h1>Produtos</h1>
+      <div className={style.produtos}>
+        <h1>Produtos</h1>
+        <Filter
+          title="Filtro"
+          searchPlaceholder="Busque por produto ou categoria..."
+          selectPlaceholder="Todos os status"
+          selectOptions={status}
+        />
+        <TableAdminComponent />
+      </div>
     </AdminLayout>
   );
 };
