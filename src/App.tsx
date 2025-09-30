a141f5d6b3b6af304784e94150bd345d33c7bdf1
Brenda Pereira <brendacamara@outlook.com>
2025-09-30 16:28:03 -0300

    chore: adicionando app.tsx de volta

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import {
+  Dashboard,
+  Estoque,
+  Produtos,
+  Pedidos,
+  Comentarios,
+  Configuracoes,
+  Home,
+  Login,
+} from '@pages';
+
+function App() {
+  return (
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/estoque" element={<Estoque />} />
+          <Route path="/produtos" element={<Produtos />} />
+          <Route path="/pedidos" element={<Pedidos />} />
+          <Route path="/comentarios" element={<Comentarios />} />
+          <Route path="/configuracoes" element={<Configuracoes />} />
+          <Route path="/login/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
+}
+
+export default App;
