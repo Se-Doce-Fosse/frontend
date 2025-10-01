7b849d2417ae8c446dc63fb298ed36aa1355ff28
Dudu-Eichner <eduardofelbereichner@gmail.com>
2025-10-01 17:58:15 -0300

    wip: página de estoque

--- src/pages/Estoque/Estoque.tsx
+++ src/pages/Estoque/Estoque.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import AdminLayout from '../../layouts/AdminLayout/AdminLayout';
+import { Filter } from '@components';
+import style from './Estoque.module.scss';
+
+const categorias = [
+  { label: 'Insumo', value: 'insumo' },
+  { label: 'Embalagem', value: 'embalagem' },
+];
 
 const Estoque: React.FC = () => {
   return (
     <AdminLayout>
-      <h1>Estoque</h1>
+      <div className={style.estoque}>
+        <h1>Estoque</h1>
+        <Filter
+          title="Filtro"
+          searchPlaceholder="Busque por um insumo ou embalagem..."
+          selectPlaceholder="Todas as categorias"
+          selectOptions={categorias}
+        />
+      </div>
     </AdminLayout>
   );
 };
