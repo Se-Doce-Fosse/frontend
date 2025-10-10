69b4e841912f58e60e80427d0f936a11e2f8df16
Isabella Becker <isabellalbecker@gmail.com>
2025-10-10 14:04:22 -0300

    feat: add intregração produtos

--- src/pages/Produtos/Produtos.tsx
+++ src/pages/Produtos/Produtos.tsx
@@ -1,22 +1,14 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './Produtos.module.scss';
 import { NavBar, Footer } from '../../components';
 import ProductList from '../../components/ProductList';
 import CartDrawerOrder from '../../components/Cart/CartDrawerOrder/CartDrawerOrder';
 import CartDrawerFinish from '../../components/Cart/CartDrawerFinish/CartDrawerFinish';
-import { TRADITIONAL_PRODUCTS, STUFFED_PRODUCTS } from '../../data/products';
+import { FEATURED_PRODUCTS } from '../../data/products';
 import { useCart } from '../../context/CartContext';
-
-const productSections = [
-  {
-    title: 'Cookies Tradicionais',
-    products: TRADITIONAL_PRODUCTS,
-  },
-  {
-    title: 'Cookies Recheados',
-    products: STUFFED_PRODUCTS,
-  },
-];
+import { fetchProducts } from '../../services/product/productService';
+import type { Category } from 'src/types/api';
+import { useNavigate } from 'react-router-dom';
 
 const Produtos = () => {
   const {
@@ -30,23 +22,86 @@ const Produtos = () => {
     quantitiesByProductId,
   } = useCart();
 
+  const navigate = useNavigate();
+
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
+  useEffect(() => {
+    let isSubscribed = true;
+
+    fetchProducts()
+      .then((data) => {
+        if (!isSubscribed) {
+          return;
+        }
+
+        setCategories(data.categories);
+        setLoading(false);
+      })
+      .catch(() => {
+        if (!isSubscribed) {
+          return;
+        }
+
+        setError('Um erro ocorreu tente novamente mais tarde');
+        setLoading(false);
+      });
+
+    return () => {
+      isSubscribed = false;
+    };
+  }, []);
+
+  const handleShowMoreClick = () => {
+    navigate('/produtos');
+  };
+
+  const renderProductSections = () => {
+    if (loading) {
+      return <div>Carregando produtos...</div>;
+    }
+
+    if (error) {
+      return <div>{error}</div>;
+    }
+
+    if (categories.length === 0) {
+      return (
+        <ProductList
+          title="Produtos em Destaque"
+          products={FEATURED_PRODUCTS}
+          showMore
+          onShowMoreClick={handleShowMoreClick}
+          onProductQuantityChange={updateProductQuantity}
+          productQuantities={quantitiesByProductId}
+        />
+      );
+    }
+
+    return categories.map((category) => (
+      <ProductList
+        key={category.id}
+        title={category.name}
+        products={category.products}
+        showMore
+        onShowMoreClick={handleShowMoreClick}
+        onProductQuantityChange={updateProductQuantity}
+        productQuantities={quantitiesByProductId}
+      />
+    ));
+  };
 
   return (
     <div className={styles.page}>
       <NavBar onCartClick={() => setActiveDrawer('order')} />
       <main className={styles.content}>
-        {productSections.map((section) => (
-          <ProductList
-            key={section.title}
-            title={section.title}
-            products={section.products}
-            onProductQuantityChange={updateProductQuantity}
-            productQuantities={quantitiesByProductId}
-          />
-        ))}
+        <div className={styles.contentContainer}>{renderProductSections()}</div>
       </main>
       <CartDrawerOrder
         open={activeDrawer === 'order'}
