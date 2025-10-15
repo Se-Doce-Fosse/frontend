76a9714bfbeac90119ad8086556d4c1da594e675
joao ramos <joaopramos076@gmail.com>
2025-10-15 18:12:51 -0300

    fix/comment-table adding table to comment page

--- src/pages/Comentarios/Comentarios.tsx
+++ src/pages/Comentarios/Comentarios.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AdminLayout from '../../layouts/AdminLayout/AdminLayout';
+import TabelAdminCommentComponent from '../../components/TempTablesComp/CommentTable/TableAdminCommentComponent/TableAdminCommentComponent';
 
 const Comentarios: React.FC = () => {
   return (
     <AdminLayout>
       <h1>Comentários</h1>
+      <TabelAdminCommentComponent />
     </AdminLayout>
   );
 };
