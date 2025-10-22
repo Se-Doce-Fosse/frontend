53f335d4fec9b8cfb6381e5f3d4401953174eefd
joao ramos <joaopramos076@gmail.com>
2025-10-22 02:08:19 -0300

    adding table in app and table on page

--- src/pages/Cupons/Cupons.tsx
+++ src/pages/Cupons/Cupons.tsx
@@ -15,3 +15,17 @@ const Cupons: React.FC = () => {
 };
 
 export default Cupons;
+
+import React from 'react';
+import AdminLayout from '../../layouts/AdminLayout/AdminLayout';
+import TabelAdminCupomComponent from '../../components/TempTablesComp/CupomTable/TableAdminCupomComponent/TableAdminCupomComponent';
+const Cupons: React.FC = () => {
+  return (
+    <AdminLayout>
+      <h1>Cupons</h1>
+      <TabelAdminCupomComponent />
+    </AdminLayout>
+  );
+};
+
+export default Cupons;
