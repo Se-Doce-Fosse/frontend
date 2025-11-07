fe82d1a933e52f0ba2e73fc1e88e3d1e40843f72
Joao Ceia Ramos <joaopramos076@gmail.com>
2025-11-07 00:00:28 -0300

    commit

--- src/views/view-controllers/OrdersDashboard.view.controller.ts
+++ src/views/view-controllers/OrdersDashboard.view.controller.ts
@@ -8,3 +8,21 @@ const fetchOrders = async () => {
     }
 };
  */
+
+/* export const getAllProductsNativeFetch = async (token: string) => {
+  const response = await fetch(`${BASE_URL}`, { // Requires explicit "headers" key
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${token}`, // Manual header configuration
+      'Content-Type': 'application/json' // Likely added automatically by the wrapper
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok'); // Manual error handling
+  }
+
+  const data = await response.json(); // Manual JSON parsing
+  return data;
+};
+ */
